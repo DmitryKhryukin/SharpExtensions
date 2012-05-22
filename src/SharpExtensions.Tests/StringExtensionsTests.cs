4b0ae3a865ff58065dcf1df8a8324f12c9fe17fa
BurundukXP <dmitry.khryukin@gmail.com>
2012-05-23 09:14:10 +1200

    Nuget Packages correction step1

--- src/SharpExtensions.Tests/StringExtensionsTests.cs
+++ src/SharpExtensions.Tests/StringExtensionsTests.cs
@@ -1,40 +1,40 @@
-﻿using NUnit.Framework;
-using FluentAssertions;
+﻿//using NUnit.Framework;
+//using FluentAssertions;
 
 namespace SharpExtensions.Tests
 {
-    [TestFixture]
-    public class StringExtensionsTests
-    {
-        public class RemoveTagsMethod
-        {
-            [Test]
-            public void NullString_ReturnEmptyString()
-            {
-                string content = null;
-                content.RemoveTags().Should().Be(string.Empty);
-            }
-
-            [Test]
-            public void EmptyString_ReturnEmptyString()
-            {
-                string content = "";
-                content.RemoveTags().Should().Be(string.Empty);
-            }
-
-            [Test]
-            public void WiteSpaceString_ReturnEmptyString()
-            {
-                string content = "   ";
-                content.RemoveTags().Should().Be(string.Empty);
-            }
-
-            [Test]
-            public void StringWithTags_RemoveTags()
-            {
-                string content = "<b>Kventin</b> Tarantino";
-                content.RemoveTags().Should().Be("Kventin Tarantino");
-            }
-        }
-    }
+//    [TestFixture]
+//    public class StringExtensionsTests
+//    {
+//        public class RemoveTagsMethod
+//        {
+//            [Test]
+//            public void NullString_ReturnEmptyString()
+//            {
+//                string content = null;
+//                content.RemoveTags().Should().Be(string.Empty);
+//            }
+//
+//            [Test]
+//            public void EmptyString_ReturnEmptyString()
+//            {
+//                string content = "";
+//                content.RemoveTags().Should().Be(string.Empty);
+//            }
+//
+//            [Test]
+//            public void WiteSpaceString_ReturnEmptyString()
+//            {
+//                string content = "   ";
+//                content.RemoveTags().Should().Be(string.Empty);
+//            }
+//
+//            [Test]
+//            public void StringWithTags_RemoveTags()
+//            {
+//                string content = "<b>Kventin</b> Tarantino";
+//                content.RemoveTags().Should().Be("Kventin Tarantino");
+//            }
+//        }
+//    }
 }
