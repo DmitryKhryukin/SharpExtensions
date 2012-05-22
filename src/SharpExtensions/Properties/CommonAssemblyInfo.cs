9fbf51119e3dc1e7e2c716183fe431e8073a6e8c
BurundukXP <dmitry.khryukin@gmail.com>
2012-05-23 11:10:46 +1200

    CommonAssemblyInfo has been added

---
+++ src/SharpExtensions/Properties/CommonAssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SharpExtensions")]
+[assembly: AssemblyCopyright("Copyright © Dmitry Khryukin 2012")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
